763e7856965662ab1cd4e52c95066ccd8d79478e
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-06-07 14:14:13 -0300

    update ex3

--- Exercises Solution/ex3V2COM.java
+++ Exercises Solution/ex3V2COM.java
@@ -1,20 +1,23 @@
 public class ex3V2COM {
   public static void main(String[] args) {
 
+    // exemplo de vetor a ser inserido
+    int[] vectorIn = new int[] { 87, 89, 1, 55, 5, 8, 5, 2, 13, 2, 9, 3, 87, 3, 2, 1, 5, 2, 2, 9, 1, 989, 13, 77, 2, 1, 8,
+        5, 88, 88, 89, 55, 989 };
 
-    int[] vectorIn = new int[] { 87, 89, 1, 55, 5, 8, 5, 2, 13, 2, 9, 3, 87, 3, 2, 1, 5, 2, 2, 9, 1, 989, 13, 2, 1, 8, 5,
-        88, 88, 89, 55 };
-
+    // execução da função criada
     int resultado = solution(vectorIn, 31);
-    System.out.println("o valor do elemento sem par é: " + resultado);
 
+    // print do resultado
+    System.out.println("o valor do elemento sem par é: " + resultado);
   }
 
+  // função criada
   private static int solution(int V[], int N) {
-    
+
     int valor = 0;
 
-    //loop para percorrer todo o vetor
+    // loop para percorrer todo o vetor
     for (int i = 0; i < V.length; i++) {
       for (int j = i + 1; j < V.length; j++) {
 
@@ -30,8 +33,6 @@ public class ex3V2COM {
         valor = V[i];
       }
     }
-
     return valor;
   }
-
 }
